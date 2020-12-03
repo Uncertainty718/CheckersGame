2a55f71bfe077c9c5a1e6ce26422c9e17ee1a044
Ucertainty718 <wellplayed553@gmail.com>
2020-12-03 17:37:55 +0300

    created a board

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ FPS = 60
 
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Checkers')
+from checkers.board import Board
 
 
 def main():
@@ -15,8 +16,20 @@ def main():
 
     while run:
         clock.tick(FPS)
+        board = Board()
         
         for event in pygame.event.get():
-            
+            """checks if any events happened"""
+            if event.type == pygame.QUIT:
+                run = False
 
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                """if we press any mouse button"""
+                pass
+
+        board.draw_squares(WIN)
+        pygame.display.update()
+
+    pygame.quit()
+            
 main()
