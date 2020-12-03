ae23325b9d2930a7757e1c53e574425f3b89aa39
Ucertainty718 <wellplayed553@gmail.com>
2020-12-03 21:25:45 +0300

    first movement of pieces

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
 
 import pygame
-from checkers.constants import WIDTH, HEIGHT
+from checkers.constants import WIDTH, HEIGHT, SQUARE_SIZE
 
 FPS = 60
 
@@ -9,14 +9,19 @@ WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Checkers')
 from checkers.board import Board
 
+def get_row_col_from_mouse(pos):
+    x, y = pos
+    row = y // SQUARE_SIZE
+    col = x // SQUARE_SIZE
+    return row, col
 
 def main():
     run = True
     clock = pygame.time.Clock()
+    board = Board()
 
     while run:
         clock.tick(FPS)
-        board = Board()
         
         for event in pygame.event.get():
             """checks if any events happened"""
@@ -25,11 +30,16 @@ def main():
 
             if event.type == pygame.MOUSEBUTTONDOWN:
                 """if we press any mouse button"""
-                pass
+                pos = pygame.mouse.get_pos()
+                row, col = get_row_col_from_mouse(pos)
+                piece = board.get_piece(row, col)
+                board.move(piece, 4, 3)
 
-        board.draw_squares(WIN)
+        board.draw(WIN)
         pygame.display.update()
 
+    
     pygame.quit()
-            
+
+
 main()
